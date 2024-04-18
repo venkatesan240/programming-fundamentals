c19a4423674261a1c3b1fc14dcfbfeef14dc55f3
venk3571 <venk3571@CSL0686.global.chainsys.com>
2024-04-18 09:29:49 +0530

    overriding

---
+++ hotal/src/com/chainsys/mobile/Samsung.java
@@ -0,0 +1,21 @@
+package com.chainsys.mobile;
+
+public class Samsung extends Mobile {
+	
+	public void processor() {
+		System.out.println("snap dragon");
+	}
+	
+	public void model() {
+		System.out.println("s11");
+	}
+	
+	public void price() {
+		System.out.println("10000");
+	}
+	
+	public void ram() {
+		System.out.println("16gb ram");
+	}
+
+}
---
+++ hotal/src/com/chainsys/mobile/TestMobile.java
@@ -0,0 +1,15 @@
+package com.chainsys.mobile;
+
+public class TestMobile {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Samsung samsung=new Samsung();
+		samsung.model();
+		samsung.processor();
+		samsung.price();
+		samsung.ram();
+		
+	}
+
+}
